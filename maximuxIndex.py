6c60f9f5eb531b90526c8762463850e06aff7a9e
Yatharth Patankar <35359160+y-patankar@users.noreply.github.com>
2024-02-25 23:55:14 -0800

    Add files via upload

---
+++ maximuxIndex.py
@@ -0,0 +1,20 @@
+def maxIndexDiff(self,a, n): 
+    ##Your code here
+    max=0
+    if n==2 and a[0]<a[1]:
+        return 1
+    else :
+        
+        for i in range(n):
+            for j in range(i+1,n):
+                if a[i]<=a[j] and i<=j and j-i>max:
+                    max=j-i
+                    
+    return max
+
+if __name__ == "__main__":
+    a=[1,12,3,41,8]
+    n=len(a)
+    print('')
+
+    # print(list(enumerate(array)))
